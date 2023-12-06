b2bc05b507147f5fdb76ea65514bc6ba320a4470
Rajdeep Singh <radewala.2@gmail.com>
2023-12-06 22:10:34 +0530

    Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+export function Header() {
+    return (
+        <header className="px-4 lg:px-6 py-5 mx-auto">
+
+            <nav className="flex flex-wrap justify-center md:justify-between items-center mx-auto max-w-screen-xl">
+
+                <a href="https://flowbite.com" className="flex justify-center items-center">
+                    <img src="/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                </a>
+
+                <ul className="flex flex-wrap mt-4 items-center font-medium flex-row lg:space-x-8 lg:mt-0">
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Marketplace</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Features</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700  hover:bg-gray-50 lg:hover:bg-transparent  lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
